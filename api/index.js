b78fd9bab4590d8a47972f7eccb678bc6eefa64d
lucasfcosta17 <lucasfcosta1709@gmail.com>
2026-03-31 15:49:35 -0300

    ajustes

--- api/index.js
+++ api/index.js
@@ -3,6 +3,17 @@ import app from "../src/app.js";
 import { initializeServices } from "../src/bootstrap.js";
 
 export default async function handler(req, res) {
+  const origin = req.headers.origin || "*";
+  res.setHeader("Access-Control-Allow-Origin", origin);
+  res.setHeader("Vary", "Origin");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+
+  if (req.method === "OPTIONS") {
+    return res.status(204).end();
+  }
+
   try {
     await initializeServices();
     return app(req, res);
